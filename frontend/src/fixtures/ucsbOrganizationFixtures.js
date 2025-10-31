70263198755637e0de49daca0bd7a822463c00a8
Yuchao Zheng <yuchaozheng@ucsb.edu>
2025-10-30 18:02:52 -0700

    add fixtues for ucsb organization

---
+++ frontend/src/fixtures/ucsbOrganizationFixtures.js
@@ -0,0 +1,34 @@
+const UCSBOrganizationFixtures = {
+    oneUCSBOrganization: {
+      id: 1,
+      orgCode: "UCSB",
+      orgTranslationShort: "UCSBBAD",
+      orgTranslation: "aaa",
+      inactive: false,
+    },
+    threeUCSBOrganizations: [
+      {
+        id: 1,
+        orgCode: "ZPR",
+        orgTranslationShort: "UCSBBAD",
+        orgTranslation: "aaa",
+        inactive: false,
+      },
+      {
+        id: 2,
+        orgCode: "SKY",
+        orgTranslationShort: "badminton",
+        orgTranslation: "UCSBbadminton",
+        inactive: false,
+      },
+      {
+        id: 3,
+        orgCode: "KRC",
+        orgTranslationShort: "tennis",
+        orgTranslation: "UCSBtennis",
+        inactive: false,
+      },
+    ],
+  };
+  
+  export { UCSBOrganizationFixtures };
