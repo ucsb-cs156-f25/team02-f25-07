55fb9bbfa93e961d5a6eaefc597f232be44fb964
Matthew Mandell <matthewmandell@ucsb.edu>
2025-11-04 17:50:40 -0800

    added fixtures HelpRequest

---
+++ frontend/src/fixtures/HelpRequestFixtures.js
@@ -0,0 +1,42 @@
+const HelpRequestFixtures = {
+  oneDate: {
+    id: 1,
+    requesterEmail: "jonn",
+    teamId: "1",
+    tableOrBreakoutRoom: "table",
+    requestTime: "2022-01-02T12:00:00",
+    explanation: "neeed help i exploded",
+    solved: false,
+  },
+  threeDates: [
+    {
+        id: 2,
+        requesterEmail: "honn",
+        teamId: "2",
+        tableOrBreakoutRoom: "table",
+        requestTime: "2022-01-02T12:00:00",
+        explanation: "need breakup advice",
+        solved: false,
+    },
+    {
+        id: 3,
+        requesterEmail: "bonn",
+        teamId: "5",
+        tableOrBreakoutRoom: "breakout",
+        requestTime: "2022-01-02T12:00:00",
+        explanation: "just wanna say hi",
+        solved: true,
+    },
+    {
+        id: 4,
+        requesterEmail: "conn",
+        teamId: "9",
+        tableOrBreakoutRoom: "breakout",
+        requestTime: "2022-01-02T12:00:00",
+        explanation: "code fails",
+        solved: false,
+    },
+  ],
+};
+
+export { HelpRequestFixtures };
