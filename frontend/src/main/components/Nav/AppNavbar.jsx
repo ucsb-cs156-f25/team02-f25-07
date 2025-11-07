0642a4b97b476876a309617fae698b1952e7fe21
Matthew Mandell <matthewmandell@ucsb.edu>
2025-11-06 18:21:52 -0800

    copiedPlaceholders

--- frontend/src/main/components/Nav/AppNavbar.jsx
+++ frontend/src/main/components/Nav/AppNavbar.jsx
@@ -68,6 +68,9 @@ export default function AppNavbar({
                   <Nav.Link as={Link} to="/articles">
                     Articles
                   </Nav.Link>
+                  <Nav.Link as={Link} to="/helprequest">
+                    HelpRequest
+                  </Nav.Link>
                   <Nav.Link as={Link} to="/menuitemreview">
                   MenuItemReview
                   </Nav.Link>
